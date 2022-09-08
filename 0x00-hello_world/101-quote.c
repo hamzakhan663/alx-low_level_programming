5c880e1e16a563dd1ea7f853db225c3793fd1b7e
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-08 15:00:48 -0700

    End of the Beginning

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - Prints out the code
+ *
+ * Return: 1 if succesful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
