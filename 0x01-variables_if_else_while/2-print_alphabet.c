3acd888ce55c781389f2d165d24ad803418bd171
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-09 14:37:16 -0700

    Solved Error

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints letters in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
