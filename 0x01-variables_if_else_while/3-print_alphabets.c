8dd1dead8a736a93d7eab4bde164fc2161c95330
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-09 15:08:16 -0700

    Solved Error

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase and uppercase
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
+
