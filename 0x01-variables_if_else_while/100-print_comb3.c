9cf2ea707ab51d03c5c5fcb65b102f3ec66baa11
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-09 16:33:42 -0700

    Solved Errors

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,6 +12,8 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
 		putchar((digit1 % 10) + '0');
 		putchar((digit2 % 10) + '0');
 
@@ -20,6 +22,8 @@ int main(void)
 
 		putchar(',');
 		putchar(' ');
+	}
+
 	}
 
 	putchar('\n');
