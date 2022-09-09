2394c8d1a500e023d32250c724cb8e7271eab8f4
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-09 15:31:15 -0700

    Solved Error

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
