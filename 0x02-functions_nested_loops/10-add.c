f41592bf2024b8bf35d126ec13fd621e288c2439
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-13 21:11:05 -0700

    Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Adds
+ * @a: Variable
+ * @b: Variable
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	_putchar(sum);
+
+	return (a + b);
+}
