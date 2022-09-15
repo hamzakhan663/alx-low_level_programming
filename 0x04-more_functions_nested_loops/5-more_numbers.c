e5f7d76e01788fda8b546ea7a7882eaf717133ea
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-15 09:53:02 -0700

    Loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,8 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			_putchar((num / 10) + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
 			_putchar((num % 10) + '0');
 		}
 
