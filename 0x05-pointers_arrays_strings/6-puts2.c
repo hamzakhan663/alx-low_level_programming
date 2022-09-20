5ccd42e801657d0b8237d9638e3ffb2005a5712f
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-20 10:06:07 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints
+ * @str: Variable
+ */
+
+void puts2(char *str)
+{
+	int index = 0;
+	int len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
