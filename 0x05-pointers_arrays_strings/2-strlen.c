cf845c3a2f3b150462ddd45bff6f7ddb7c4463fd
hamzakhan663 <suleymanhamza63@gmail.com>
2022-09-20 01:44:22 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns Length
+ * @s: Variable
+ *
+ * Return: Lenght
+ */
+
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+i
+	return (length);
+}
